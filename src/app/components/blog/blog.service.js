595391ffbb8366dbfdb34ab3cf0a2916acd80250
k <brood915@gmail.com>
2017-06-20 14:52:40 -0400

    removed decorations from links

--- src/app/components/blog/blog.service.js
+++ src/app/components/blog/blog.service.js
@@ -2,10 +2,11 @@ export class BlogService {
     /* @ngInject */
   constructor($http) {
     this.$http = $http;
+    this.blogItems = null;
   }
 
   getData () {
     return this.$http.get('blog.json')
     .then((resp)=> resp.data)
   }
-}
+} 
