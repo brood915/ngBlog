9e1a6d7f19125677635c32899ac852e6f5ae6ae8
brood915 <brood915@gmail.com>
2017-06-21 21:54:30 -0400

    added a comment form

--- src/app/components/blog/blog-detail/blog-detail.component.js
+++ src/app/components/blog/blog-detail/blog-detail.component.js
@@ -15,11 +15,26 @@ class BlogDetailCtrl {
     if (this.blogItems){
     //find the blogitem with the same id as the one passed to param
     this.item = this.blogItems.find((each) => each.id === Number(this.$stateParams.blogId));
-    }
+    this.resetComment(); //to get the next id # of comment initially
+  }
     else { //if blogitem not found
       this.$state.go('404'); //redirect to /404 to display error message
     }
   }
+
+  addComment () {
+    this.item.comments.push(this.comment);
+    this.resetComment(); //to get the next id # after adding a comment
+  }
+
+ resetComment () {
+   this.comment = {
+     "id": this.item.comments.length +1,
+     "text": ""
+   }
+ }
+
+
 }
 
 
