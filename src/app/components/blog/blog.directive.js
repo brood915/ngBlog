d93e07a592b95c2c7606edf5aab6e32904affb3e
brood915 <brood915@gmail.com>
2017-06-23 19:49:06 -0400

    added logics to prevent content from getting pushed down when modal is open

--- src/app/components/blog/blog.directive.js
+++ src/app/components/blog/blog.directive.js
@@ -10,12 +10,16 @@ class BlogDirective {
   link(scope, element, attrs) { //remember that scope value is parsed and evaluted while attrs is always string.
       element.on('click', function() {
           let form = document.getElementById('formContainer');
-         if (form.classList.contains("hideModal"))
+          let content = document.getElementById('blogItems');
+
+         if (form.classList.contains("hide"))
           {
-              form.classList.remove("hideModal");
+              form.classList.remove("hide");
+              content.classList.add("hide");
           }
           else {
-              form.classList.add("hideModal");
+              form.classList.add("hide");
+              content.classList.remove("hide");
           }
       })
   }
