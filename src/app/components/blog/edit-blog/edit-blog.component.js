f75b19297b96dfd611d80ad0b1afb4fcce05c61c
k <brood915@gmail.com>
2017-06-26 16:37:48 -0400

    working on the edit functionality

--- src/app/components/blog/edit-blog/edit-blog.component.js
+++ src/app/components/blog/edit-blog/edit-blog.component.js
@@ -1,22 +1,26 @@
 import template from './edit-blog.html';
+import angular from 'angular';
 
 class EditBlogCtrl {
     /* @ngInject */
   constructor(blogService,$scope) {
       this.blogService = blogService;
-      this.$scope = $scope;
   }
 
   $onInit() {
-
+    this.modifiedBlog = angular.copy(this.blogItem);
   }
 
+  editBlog () {
+      this.blogItem = this.modifiedBlog;
+      console.log(this.blogItem);
+  }
 }
 
 
 export const EditBlogComponent = {
   controller: EditBlogCtrl,
   template,
-  bindings: {}
+  bindings: {blogItem: '<'}
 }
     
