a95e8a23cf14243dd3bb31b7977c4f114088483f
k <brood915@gmail.com>
2017-06-27 16:55:42 -0400

    added custom sort function to sort blog posts by date

--- src/app/components/blog/blog.component.js
+++ src/app/components/blog/blog.component.js
@@ -6,21 +6,42 @@ class BlogCtrl {
   constructor(blogService, $scope) {
     this.blogService = blogService;
     this.$scope = $scope;
+    this.$scope.$watch(()=>this.sortBy, ()=>this.handleSort());
   }
 
   $onInit() {
     this.filterValue = "";
     this.selectOptions = ['title', 'oldest', 'recent'];
-    this.sortBy = 'recent';
-    
+
     if (!this.blogService.blogItems){ //initialize data if not present yet
       this.blogService.getData()
-      .then((resp) => {this.blogService.blogItems = resp; this.blogItems = resp;})
+      .then((resp) => 
+      {
+        this.blogService.blogItems = resp; 
+        this.blogItems = resp;
+        this.sortBy = 'recent';
+      });
     }
     else {
       this.blogItems = this.blogService.blogItems;
+      this.sortBy = 'recent';
     }
   }
+
+
+  handleSort (){
+      if (this.sortBy === 'oldest') {
+        this.blogItems.sort(function(a,b){
+          return new Date(a.date) - new Date(b.date);
+        });
+      }
+      else if (this.sortBy === 'recent') {
+        this.blogItems.sort(function(a,b){
+          return new Date(b.date) - new Date(a.date);        
+      });
+    }
+  }
+
 
   deleteBlog (index) {
    this.$scope.$apply(()=>{
