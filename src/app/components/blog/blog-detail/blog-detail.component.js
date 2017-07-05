3a63e5fa4469ddb87726f9739dba8093c503bc6f
k <brood915@gmail.com>
2017-07-05 16:35:03 -0400

    allows users to reply to comments

--- src/app/components/blog/blog-detail/blog-detail.component.js
+++ src/app/components/blog/blog-detail/blog-detail.component.js
@@ -1,4 +1,5 @@
 import template from './blog-detail.html';
+import angular from "angular";
 
 class BlogDetailCtrl {
     /* @ngInject */
@@ -50,36 +51,62 @@ class BlogDetailCtrl {
   addComment () {
     this.item.comments.push(this.comment);
     this.resetComment(); //to get the next id # after adding a comment
-    this.$timeout (() => 
-      this.$scope.addCommentForm.$setPristine()
-    ); //resets form
+  }
+
+  startReplying (index) {
+    this.resetReply(index);
+    this.item.comments[index].replying = !this.item.comments[index].replying;
+  }
+
+  addReply (index) {
+    let reply = angular.copy(this.item.comments[index].reply);
+    this.item.comments[index].replies.push(reply);
+    this.item.comments[index].seeReplies = true; //shows replies after adding reply to the comment
+    this.resetComment(); 
+    this.resetReply(index);
+  }
+
+   deleteComment (index) {
+    this.$scope.$apply(this.item.comments.splice(index,1)); //apply used to allow confirm directive to update view after running this
+    this.item.comments.map((each,index)=> each.id = index); //change the id #s to prevent duplicates
+    this.resetComment();
+ }
+
+  deleteReply (ind) {
+    const replies = this.item.comments[ind].replies;
+    this.$scope.$apply(replies.splice(ind,1));
+    replies.map((each,index)=> each.id = index);
+    this.resetComment();
+    this.resetReply(ind);   
+  }
+
+  resetReply (index) {
+    this.item.comments[index].reply = {
+      "id": this.item.comments[index].replies.length,
+      "text": "",
+      "name": "",
+      "date":"",
+    }
   }
 
  resetComment () {
    this.comment = {
      "id": this.item.comments.length,
      "text": "",
-     "comments":"",
+     "replies":[],
      "name":"",
      "date" : "",
      "expanding": false,
-     "replying": false
+     "replying": false,
+     "seeReplies": false
    }
  }
-
- deleteComment (index) {
-   this.$scope.$apply(this.item.comments.splice(index,1)); //to allow confirm directive to update view after running this
-   this.item.comments.map((each,index)=> each.id = index); //change the id #s to prevent duplicates
-   this.resetComment();
- }
-
-
 }
 
 
 export const BlogDetailComponent = {
   controller: BlogDetailCtrl,
   template,
-  bindings: {index:'<'}
+  bindings: {}
 }
     
