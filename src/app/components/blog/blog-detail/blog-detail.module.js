c907272d31d0114a7cee4f1153ae8dc71fdeefee
k <brood915@gmail.com>
2017-07-06 10:44:18 -0400

    converted comment section to a component

--- src/app/components/blog/blog-detail/blog-detail.module.js
+++ src/app/components/blog/blog-detail/blog-detail.module.js
@@ -1,7 +1,8 @@
 import angular from 'angular';
 import { BlogDetailComponent } from './blog-detail.component';
+import { AddCommentsModule } from './add-comments/add-comments.module'
 import './blog-detail.scss';
 
-export const BlogDetailModule = angular.module('blogDetailModule', [])
+export const BlogDetailModule = angular.module('blogDetailModule', [AddCommentsModule])
     .component('blogDetail', BlogDetailComponent)
     .name;
