2624b924856f393b9e7d8ebb3d8fa8fc6de11f8a
k <brood915@gmail.com>
2017-08-08 15:31:18 -0400

    added db.js

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', (err) => {
+  console.error(`${err.message}`);
+});
--- server/server.js
+++ server/server.js
@@ -1,16 +1,5 @@
-const mongoose = require('mongoose');
 const app = require('./app');
 
-
-//database
-mongoose.connect(process.env.DATABASE);
-mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connection.on('error', (err) => {
-  console.error(`${err.message}`);
-});
-
-
-//server
 app.set('port', process.env.PORT || 1337);
 
 const server = app.listen(app.get('port'), () => {
