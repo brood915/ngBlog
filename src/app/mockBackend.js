3a82f904599e60f84e3f8990c1d9018e47187bc2
brood915 <brood915@gmail.com>
2017-08-13 23:47:05 -0400

    added ngmock config

--- src/app/mockBackend.js
+++ src/app/mockBackend.js
@@ -4,12 +4,22 @@ export default function($httpBackend) {
     /* @ngInject */
   $httpBackend.whenGET('/posts').respond(posts);
 
-    $httpBackend.whenGET('/post').respond(() => {
-    return [200, posts, {}];
-  });
-
-  $httpBackend.whenPOST('/posts').respond((method, url, data) => {
+  $httpBackend.whenPOST('/posts/add').respond((method, url, data) => {
     posts.push(JSON.parse(data));
     return [200, posts, {}];
   });
+
+  $httpBackend.whenRoute('PUT', '/posts/edit/:id')
+    .respond((method, url, data, headers, params) => {
+      const updated = posts.find((each,index) => each.id === params.id);
+      const index = posts.indexOf(updated);
+      posts[index] = data;
+      return [200, posts];
+    });
+
+  $httpBackend.whenRoute('DELETE', '/posts/delete/:id')
+    .respond((method, url, data, headers, params) => {
+      const afterDelete = posts.filter((each) => each.id !== params.id);
+      return [200, afterDelete];
+    });
 }
