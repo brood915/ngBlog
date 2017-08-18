6def0783846cc6df98fad92b946336658cf80ba2
brood915 <brood915@gmail.com>
2017-08-17 21:52:35 -0400

    added env sample file for testing. changed config for server

--- server/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-
-app.set('port', process.env.PORT || 1337);
-
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
