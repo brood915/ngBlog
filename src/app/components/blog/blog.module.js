3b03f1676109630599a2f691701dde6ac0aa12c2
brood915 <brood915@gmail.com>
2017-10-02 19:47:07 -0400

    added typeahead search result component

--- src/app/components/blog/blog.module.js
+++ src/app/components/blog/blog.module.js
@@ -5,8 +5,9 @@ import { BlogDetailModule } from './blog-detail/blog-detail.module';
 import { BlogService } from './blog.service';
 import { AddBlogModule } from './add-blog/add-blog.module';
 import { EditBlogModule } from './edit-blog/edit-blog.module';
+import { SearchModule } from './search/search.module';
 
-export const BlogModule = angular.module('blogModule', [BlogDetailModule, AddBlogModule, EditBlogModule])
+export const BlogModule = angular.module('blogModule', [BlogDetailModule, AddBlogModule, EditBlogModule , SearchModule])
     .component('blog', BlogComponent)
     .service('blogService', BlogService)
     .name;
